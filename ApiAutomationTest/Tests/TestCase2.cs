55519c2dafe5266fbb4a1ff74b03dafee3d6e177
jtmartinez <jtmartinez@unionbank.com.ph>
2023-07-09 19:48:15 +0800

    Tc2

---
+++ ApiAutomationTest/Tests/TestCase2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SeleniumAutomationTest.Pages;
+
+namespace SeleniumAutomationTest.Tests
+{
+    public class TestCase2 : TestBase
+    {
+        private TestCase1 testCase1 = new TestCase1();
+        [Test]
+        public void LoginUserWithCorrectEmailAndPassword()
+        {
+            testCase1.VerifyHomePageIsVisible();
+            VerifyLoginToYourAccountIsVisible();
+        }
+
+        public void VerifyLoginToYourAccountIsVisible()
+        {
+            string loginToYourAccountTxt = new HomePage(GetDriver())
+                .SignUpLinkClick()
+                .GetLoginToYourAccount()
+                .Text;
+            Assert.AreEqual("Login to your account", loginToYourAccountTxt);
+        }
+    }
+}
---
+++ ApiAutomationTest/Utils/JSONUtil.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace SeleniumAutomationTest.Utils
+{
+    public class JSONUtil
+    {
+        public static void WriteToJsonFile(string json, string filePath)
+        {
+            File.WriteAllText(filePath, json);
+        }
+        public static void WriteObjectToJsonFile(object obj, string filePath)
+        {
+            string json = JsonConvert.SerializeObject(obj, Formatting.Indented);
+            File.WriteAllText(filePath, json);
+        }
+    }
+}
